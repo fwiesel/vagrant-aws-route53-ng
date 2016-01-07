ffb40420765ce3e784f2e159b3f80956902b72d3
Ed Ropple <ed@edropple.com>
2016-01-07 14:17:22 -0500

    added hook to :machine_action_resume

--- lib/vagrant-aws-route53-ng/plugin.rb
+++ lib/vagrant-aws-route53-ng/plugin.rb
@@ -19,6 +19,12 @@ module VagrantPlugins
         hook.after VagrantPlugins::AWS::Action::StartInstance, VagrantPlugins::Route53NG::Action::SetIp
       end
 
+      action_hook :assign_ip_to_route53, :machine_action_resume do |hook|
+        require_relative './action/set_ip'
+        hook.after VagrantPlugins::AWS::Action::RunInstance,   VagrantPlugins::Route53NG::Action::SetIp
+        hook.after VagrantPlugins::AWS::Action::StartInstance, VagrantPlugins::Route53NG::Action::SetIp
+      end
+
       action_hook :cancel_ip_from_route53, :machine_action_halt do |hook|
         require_relative './action/unset_ip'
         hook.after VagrantPlugins::AWS::Action::StopInstance,      VagrantPlugins::Route53NG::Action::UnsetIp
